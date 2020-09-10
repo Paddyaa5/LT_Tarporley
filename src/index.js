61cf806429f3bd858f2d7c35b8aa79d0cad5f8a7
PaddyAA <Patrick_aanderson@hotmail.co.uk>
2020-09-10 09:40:15 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  first: '#ECE9D5', //Eggshell
+  second: '#18323A', //Gunmetal
+  third: '#464C49', //Grey
+  
+  font1: "'Landmark-Dimensional', sans-serif",
+  font2: "'Bebas-Regular', sans-serif",
+  font3: "'Poppins', sans-serif",
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
